21a7599b208354c159286edfaa990b7219337af6
Mafeking Ihimaera <mafe.smiler@gmail.com>
2026-07-01 20:52:30 +1200

    Disable PostCSS auto-config search to avoid Windows BOM crash

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
